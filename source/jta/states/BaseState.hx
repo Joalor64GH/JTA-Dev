e4c1c12566f793dab093c24ad9cdfd887fffebe8
Joalor64 <rhgarcon@gmail.com>
2025-08-28 08:46:55 -0400

    mod menu test

--- source/jta/states/BaseState.hx
+++ source/jta/states/BaseState.hx
@@ -8,8 +8,8 @@ import flixel.graphics.FlxGraphic;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
 import jta.registries.ModuleRegistry;
-import jta.mobile.MobilePad;
 import jta.states.Startup;
+import jta.input.Input;
 import jta.Data;
 
 /**
@@ -18,8 +18,6 @@ import jta.Data;
  */
 class BaseState extends FlxTransitionableState
 {
-	var controls:MobilePad;
-
 	/**
 	 * @param noTransition Whether or not to skip the transition when entering a state.
 	 */
@@ -52,9 +50,6 @@ class BaseState extends FlxTransitionableState
 	{
 		super.create();
 
-		controls = new MobilePad();
-		add(controls);
-
 		for (module in ModuleRegistry.getAllModules(true))
 		{
 			module.create();
@@ -68,8 +63,6 @@ class BaseState extends FlxTransitionableState
 		if (FlxG.save.data != null)
 			FlxG.fullscreen = Data.settings.fullscreen;
 		#end
-
-		controls.updateInput();
 
 		super.update(elapsed);
 
---
+++ source/jta/substates/ModMenu.hx
@@ -0,0 +1,114 @@
+package jta.substates;
+
+import flixel.FlxObject;
+import jta.modding.PolymodHandler;
+import jta.substates.BaseSubState;
+
+class ModMenu extends BaseSubState
+{
+    var mods:FlxTypedGroup<FlxText>;
+    var icons:FlxTypedGroup<ModIcon>;
+    var desc:FlxText;
+    var selectedIndex:Int = 0;
+    var camera:FlxObject;
+
+    public function new():Void
+    {
+        super(0x80000000);
+    }
+
+    override public function create():Void
+    {
+        cameras = [FlxG.cameras.list[1]];
+
+        camera = new FlxObject(80, 0, 0, 0);
+        camera.screenCenter(X);
+        add(camera);
+
+        if (PolymodHandler.trackedMods.length > 0){
+
+        mods = new FlxTypedGroup<FlxText>();
+        add(mods);
+
+        for (i in 0...PolymodHandler.trackedMods.length)
+        {
+            var text:FlxText = new FlxText(10, 100 + i * 42, FlxG.width, PolymodHandler.trackedMods[i].title);
+            text.setFormat(Paths.font('main'), 36, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+            text.scrollFactor.set();
+            text.ID = i;
+            add(text);
+        }
+        } else {
+            var title:FlxText = new FlxText(10, 10, FlxG.width, "NO MODS INSTALLED!");
+		title.setFormat(Paths.font('main'), 40, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		title.scrollFactor.set();
+		add(title);
+        }
+
+        super.create();
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        if (Input.justPressed('confirm'))
+		{
+					FlxG.sound.play(Paths.sound('select'));
+					close();
+		}
+
+        super.update(elapsed);
+    }
+}
+
+class ModIcon extends FlxSprite
+{
+	public var sprTracker:FlxSprite;
+
+	public function new(bytes:haxe.io.Bytes):Void
+	{
+		super();
+
+		if (bytes != null && bytes.length > 0)
+		{
+			try
+			{
+				var bmp:BitmapData = BitmapData.fromBytes(bytes);
+				var sheetWidth = bmp.width;
+				var sheetHeight = bmp.height;
+				var frameWidth = 150;
+				var frameHeight = 150;
+				var framesWide = Math.floor(sheetWidth / frameWidth);
+				var framesHigh = Math.floor(sheetHeight / frameHeight);
+				var totalFrames = framesWide * framesHigh;
+
+				loadGraphic(bmp, true, frameWidth, frameHeight);
+
+				var frameIndices = [for (i in 0...totalFrames) i];
+				animation.add('icon', frameIndices, 24, true);
+				animation.play('icon');
+			}
+			catch (e:Dynamic)
+			{
+				FlxG.log.warn(e);
+				loadGraphic(Paths.image('errorSparrow'));
+			}
+		}
+		else
+			loadGraphic(Paths.image('errorSparrow'));
+
+		setGraphicSize(75, 75);
+		scrollFactor.set();
+		updateHitbox();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+		{
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y);
+			scrollFactor.set(sprTracker.scrollFactor.x, sprTracker.scrollFactor.y);
+		}
+	}
+}
