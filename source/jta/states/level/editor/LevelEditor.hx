8f724d76f5c7aa9d8459a311b9183d1ba36b85df
Joalor64 <rhgarcon@gmail.com>
2025-09-09 20:13:25 -0400

    more wip editor stuff + french locale

--- source/jta/states/level/editor/LevelEditor.hx
+++ source/jta/states/level/editor/LevelEditor.hx
@@ -1,20 +1,65 @@
 package jta.states.level.editor;
 
+import jta.Paths;
+import jta.input.Input;
+import jta.states.MainMenu;
+import jta.objects.level.Player;
+import jta.objects.level.Object;
+import jta.registries.level.PlayerRegistry;
+import jta.registries.level.ObjectRegistry;
+import jta.registries.LevelRegistry;
+import openfl.events.Event;
+import openfl.events.IOErrorEvent;
+import openfl.net.FileReference;
+import openfl.net.FileFilter;
+import flixel.tile.FlxTilemap;
+import flixel.FlxCamera;
+
+/**
+ * A state dedicated to allowing the user to create and edit levels.
+ * Built with HaxeUI for use by both developers and modders.
+ * @author Joalor64
+ */
+// TO-DO: Add more functionality (loading and opening CSV/level script files, placing objects/tiles and player)
+// MAYBE a script editor too??? Hot-reloading???? 
 @:build(haxe.ui.ComponentBuilder.build("assets/ui/main-view.xml"))
 class LevelEditor extends haxe.ui.backend.flixel.UIState
 {
-    override function create()
+    var player:Player;
+    var objects:FlxTypedGroup<Object>;
+
+    var camHUD:FlxCamera;
+    var camEditor:FlxCamera;
+
+    private override function create():Void
     {
+        FlxG.mouse.visible = true;
+
+        var gridLine:FlxSprite = new FlxSprite();
+		var gfx = gridLine.makeGraphic(FlxG.width, FlxG.height, 0x00000000, true);
+		for (x in 0...Std.int(FlxG.width / 32) + 1)
+		{
+			gfx.pixels.fillRect(new openfl.geom.Rectangle(x * 32, 0, 1, FlxG.height), 0x55FFFFFF);
+		}
+		for (y in 0...Std.int(FlxG.height / 32) + 1)
+		{
+			gfx.pixels.fillRect(new openfl.geom.Rectangle(0, y * 32, FlxG.width, 1), 0x55FFFFFF);
+		}
+		gridLine.pixels = gfx.pixels;
+		gridLine.dirty = true;
+		add(gridLine);
+
         super.create();
     }
 
-    override function update(elapsed:Float)
+    private override function update(elapsed:Float):Void
     {
         super.update(elapsed);
 
-        if (FlxG.keys.justPressed.ESCAPE)
+        if (Input.justPressed('cancel'))
         {
-            FlxG.switchState(new jta.states.MainMenu());
+            FlxG.mouse.visible = false;
+            FlxG.switchState(new MainMenu());
         }
     }
 }
